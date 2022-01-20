9ac7ae1dcc5f4a7286a0b1f687a18f612e61e13c
ahsanaman883 <80390474+ahsanaman883@users.noreply.github.com>
2022-01-20 05:02:44 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+
+
+
+
+
+
+
+
+
+
+var toggle = document.querySelector(".toggle");
+var nav = document.querySelector(".nav");
+
+toggle.addEventListener("click", function () {
+    toggle.classList.toggle("active");
+    nav.classList.toggle("active");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
